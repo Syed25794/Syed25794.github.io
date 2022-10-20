d34fb78745bc975e247e10c2b74a2a8b5a03736e
Syed25794 <syedgulmohammad25794@gmail.com>
2022-10-20 13:56:44 +0530

    added about contact project etc. components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import { Box} from "@chakra-ui/react";
-import { Navbar } from "./Components/Navbar";
+import { Box } from "@chakra-ui/react";
+import {
+  Navbar,
+  About,
+  Contact,
+  Projects,
+  Resume,
+  Skills,
+} from "./Components/Navbar";
 
 function App() {
   return (
     <Box>
       <Navbar />
+      <About />
+      <Skills />
+      <Projects />
+      <Resume />
+      <Contact />
     </Box>
   );
 }
