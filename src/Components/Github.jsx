8d20622503a00d219f7e0835e4a492114f3adb13
Syed25794 <syedgulmohammad25794@gmail.com>
2022-11-15 19:51:05 +0530

    respnosiveness of github box

--- src/Components/Github.jsx
+++ src/Components/Github.jsx
@@ -4,13 +4,13 @@ import { FaGithub } from 'react-icons/fa';
 
 const Github = () => {
     return (
-        <Box>
+        <Box id='projects' boxShadow ='rgb(127,127,127) 0px 5px 15px' mx={['15px','25px','50px']} my='90px' p={['10px','15px','25px']} borderRadius='8px'>
             <Box color='white' textAlign='center' mb={6}>
                 <Heading>My GitHub Stats</Heading>
                 <Icon w="50px" h="50px" mt={3}  as={FaGithub} />
             </Box>
-            <Flex direction='column' gap={4} wrap='wrap' justifyContent='center'>
-                <Box width={['400px','600px','800px']}>
+            <Flex direction='column' gap={4} wrap='wrap' justifyContent='center' align='center' >
+                <Box width={['300px','600px','800px']}>
                     <Image src='https://activity-graph.herokuapp.com/graph?username=Syed25794' alt='github activity' / >
                 </Box>
                 <Flex direction={['column','row','row']} gap={4} wrap='wrap'>   
