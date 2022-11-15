353cd8409832864a72691d9d08e0a781ebc3eb45
Syed25794 <syedgulmohammad25794@gmail.com>
2022-11-16 05:28:57 +0530

    contact section alignment

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -2,22 +2,22 @@ import {Box, Button, Flex, Heading, Input, Text} from '@chakra-ui/react'
 
 export const Contact=()=>{
     return (
-        <Flex direction={['column','row','row']} id='contact' padding={10} >
+        <Flex direction={['column','row','row']} id='contact' padding={10} justifyContent='center'>
             <Box id='projects' boxShadow ='rgb(127,127,127) 0px 5px 15px' mx={['15px','25px','50px']} my='90px' p={['10px','15px','25px']} borderRadius='8px' justifyContent='center'>
                 <Heading>Contact me</Heading>
                 <Box p={5}>
                     <Text marginBottom={2}>Name</Text>
-                    <Input w="250px" />
+                    <Input w={["200px","225px","250px"]} />
                 </Box>
                 <Box p={5}>
                     <Text marginBottom={2}>Email</Text>
-                    <Input w="250px" />
+                    <Input w={["200px","225px","250px"]} />
                 </Box>
                 <Box p={5}>
                     <Text marginBottom={2}>Message</Text>
-                    <Input w="250px" />
+                    <Input w={["200px","225px","250px"]} />
                 </Box>
-                <Box marginLeft='20px'>
+                <Box marginLeft={['35px','45px','65px']}>
                     <Button>Connect With Me</Button>
                 </Box>
             </Box>
