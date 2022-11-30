783c3b96c8fbcf1d7f530055addeb53f6a45f721
Syed25794 <syedgulmohammad25794@gmail.com>
2022-11-30 19:46:20 +0530

    github section almost done

--- src/Components/Github.jsx
+++ src/Components/Github.jsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Heading, Icon, Image, Link} from '@chakra-ui/react';
+import { Box, Heading, Icon, Image, Link, Text} from '@chakra-ui/react';
 import React from 'react';
 import { FaGithub } from 'react-icons/fa';
 
@@ -9,26 +9,30 @@ const Github = () => {
                 <Heading fontSize={['2xl','3xl','3xl']} fontWeight='500' >My GitHub Stats</Heading>
                 <Icon w="50px" h="50px" mt={3}  as={FaGithub} />
             </Box>
-            <Flex direction='column' gap={4} wrap='wrap' justifyContent='center' align='center' >
-                <Box width={['300px','600px','800px']}>
-                    <Image bg='gray' src='https://activity-graph.herokuapp.com/graph?username=Syed25794&bg_color=0D1117' alt='github activity' / >
-                </Box>
-                <Flex direction={['column','row','row']} gap={4} wrap='wrap'> 
-                    <Box><Image src="https://github-profile-trophy.vercel.app/?username=syed25794" alt="syed25794" /></Box>
-                    <Box bg='gray'><Link href="https://github.com/ryo-ma/github-profile-trophy"></Link></Box>
-                    <Box><Image src="https://github-readme-streak-stats.herokuapp.com/?user=syed25794&" alt="syed25794" />  </Box>
-                </Flex>
-                <Box>
+            <Box margin='auto'>
+                <Image bg='#171923' src='https://activity-graph.herokuapp.com/graph?username=Syed25794&bg_color=#9E9E9E' alt='github activity' / >
+            </Box>
+            <Box  w={['250px','350px','600px','800px']} margin='auto' marginTop='30px'>
+                <Text textAlign='center' fontSize='25px' color='white' >Github Trophies</Text>
+                <Image w={['250px','350px','600px','800px']} marginTop='10px' src="https://github-profile-trophy.vercel.app/?username=syed25794" alt="syed25794" />
+                <Link href="https://github.com/ryo-ma/github-profile-trophy"></Link>
+            </Box>
+            <Box margin='auto' marginTop='30px' w={['250px','350px','600px','800px']} >
+                <Text textAlign='center' fontSize='25px' color='white' >Github Streaks</Text>
+                <Image marginTop='10px' w={['250px','350px','600px','800px']} src="https://github-readme-streak-stats.herokuapp.com/?user=syed25794&" alt="syed25794" />
+            </Box>
+            <Box margin='auto' marginTop='30px'w={['250px','350px','600px','800px']} textAlign='center'>
+                <Link target="_blank" rel="noreferrer" id="cicin" href="https://github.com/syed25794">
+                    <Text textAlign='center' fontSize='25px' color='white' >Github Statistics</Text>
+                    <Image w={['250px','350px','600px','800px']} marginTop='10px' marginBottom='20px' src="https://github-readme-stats.vercel.app/api?username=syed25794&show_icons=true&count_private=true&theme=react&hide_border=true&bg_color=0D1117"  id="github-contributions" href="https://github.com/syed25794/github-readme-stats" alt="syed25794's Github Stats"/>
+                </Link>
+            </Box>
+            <Box>
                 <Link target="_blank" rel="noreferrer" id="cicin" href="https://github.com/syed25794" >
                     <p textAlign='center' fontSize='40px' color='black' >Github Calender</p>
                     <Image  width="1200px" marginBottom='20px'  id="github-contributions" src="https://ghchart.rshah.org/syed25794" href="https://github.com/syed25794" alt="syed25794's Activity Graph" />
                 </Link>
-                <Link target="_blank" rel="noreferrer" id="cicin" href="https://github.com/syed25794">
-                    <p textAlign='center' fontSize='40px' color='black' >Github Statistics</p>
-                    <img marginBottom='20px' src="https://github-readme-stats.vercel.app/api?username=syed25794&show_icons=true&count_private=true&theme=react&hide_border=true&bg_color=0D1117"  id="github-contributions" href="https://github.com/syed25794/github-readme-stats" alt="syed25794's Github Stats"/>
-                </Link>
-                </Box>
-            </Flex>
+            </Box>
         </Box>
     )
 };
